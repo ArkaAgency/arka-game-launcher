46f42d53e71a7e8b219fc3e5742456848ab83de0
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-07-29 16:39:55 +0200

    :triangular_flag_on_post: Added page Redux Action

--- src/features/page.action.js
+++ src/features/page.action.js
@@ -2,3 +2,15 @@
 export const DEFINE_PAGE = 'plage/define';
 
 // it's defining the actions methods
+
+/**
+ * Define the new app page
+ * @date 7/29/2023 - 4:08:01 PM
+ *
+ * @param {string} page
+ * @returns {{ type: string; payload: any; }}
+ */
+export const definePage = (page) => ({
+    type: DEFINE_PAGE,
+    payload: page
+});
