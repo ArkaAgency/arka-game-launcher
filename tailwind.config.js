ef5432bef1b73cb5570b513ede28537ddc80d347
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-07-30 22:58:25 +0200

    :lipstick: Added some CSS things and updated tailwind cfg

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
         './src/**/*.{js,jsx,ts,tsx}'
     ],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                'mc-server-backdrop': 'url(\'./../public/mc-server-backdrop.gif\')'
+            }
+        },
     },
     plugins: [],
 };
