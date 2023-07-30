8bd2a427145a418c40b68fb713251d51f3af4697
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-07-30 23:02:21 +0200

    :triangular_flag_on_post: Added Shop temporary error message

---
+++ src/views/Shop.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export function Shop() {
+    return <div className='h-full w-full flex items-center justify-center'>
+        <img src="./sad-creeper.png" alt="sad creeper image" className='w-[400px] h-auto' />
+        <p>Nous sommes désolés. La <strong>Boutique Arka</strong> est momentanément indisponible.</p>
+    </div>;
+}
