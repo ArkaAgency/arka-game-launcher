264bb05436d77c704fee40f8a161c5c66ab5a413
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-07-30 07:45:56 +0200

    :bug: Removed React strict mode

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <>
         <StoreProvider store={store}>
             <AppRouter />
         </StoreProvider>
-    </React.StrictMode>
+    </>
 );
