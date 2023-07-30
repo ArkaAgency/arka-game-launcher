d2ac54bad724943212e7bcdc14a4d65e8a2c504e
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-07-30 22:55:08 +0200

    :triangular_flag_on_post: Added Sidebar player username and fixed some menu items

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import { FiBox, FiMail, FiSettings } from 'react-icons/fi';
+import React, { useEffect, useState } from 'react';
+import { FiHexagon, FiShoppingCart, FiSettings } from 'react-icons/fi';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectPage } from '../utils/selectors';
 import { PAGES } from '../constants/PAGES';
 import { definePage } from '../features/page.action';
+import { getUserData } from '../utils/config';
 
 export function Sidebar(){
+    const [username, setUsername] = useState('');
+
     const page = useSelector(selectPage());
     const dispatch = useDispatch();
 
+    useEffect(() => {
+        const userData = getUserData() || {profile:{name:'PLAYERNAME'}};
+        setUsername(userData.profile.name);
+    }, [page]);
+
     const handlePageChange = (e) => {
-        if (e.target.dataset.page === page) return;
-        dispatch(definePage(e.target.dataset.page));
+        console.log(e.currentTarget.getAttribute('data-page'));
+        if (e.currentTarget.dataset.page === page) return;
+        dispatch(definePage(e.currentTarget.dataset.page));
     };
 
     return <div className={'flex-shrink-0 w-40 h-full flex flex-col' + ([
@@ -21,20 +30,20 @@ export function Sidebar(){
         <div className='w-full'>
             <img src='./icon.png' alt='Arka Group Icon' className='w-[45px]' />
         </div>
-        <ul className='flex flex-col w-full mt-5'>
-            <li className={'m-2 text-slate-500 text-sm font-bold flex items-center w-full cursor-pointer hover:text-white transition-all' + (page === PAGES.CONTENT_MODDED ? ' text-white' : '')} onClick={handlePageChange} data-page={PAGES.CONTENT_MODDED}>
-                <FiBox className='text-2xl' />&nbsp;&nbsp;&nbsp;Modded&nbsp;&nbsp;<span className='p-1 rounded bg-slate-800 text-xs font-bold'>S1</span>
+        <ul className='flex flex-col w-full mt-10'>
+            <li className={'m-2 mb-4 text-slate-500 text-sm font-bold flex items-center w-full cursor-pointer hover:text-white transition-all' + (page === PAGES.HOME ? ' text-white' : '')} onClick={handlePageChange} data-page={PAGES.HOME}>
+                <FiHexagon className='text-2xl' />&nbsp;&nbsp;&nbsp;Accueil
             </li>
-            <li className={'m-2 text-slate-500 text-sm font-bold flex items-center w-full cursor-pointer hover:text-white transition-all' + (page === PAGES.CONTENT_NEWS ? ' text-white' : '')} onClick={handlePageChange} data-page={PAGES.CONTENT_NEWS}>
-                <FiMail className='text-2xl' />&nbsp;&nbsp;&nbsp;Nouveautés
+            <li className={'m-2 mb-4 text-slate-500 text-sm font-bold flex items-center w-full cursor-pointer hover:text-white transition-all' + (page === PAGES.SHOP ? ' text-white' : '')} onClick={handlePageChange} data-page={PAGES.SHOP}>
+                <FiShoppingCart className='text-2xl' />&nbsp;&nbsp;&nbsp;Boutique
             </li>
         </ul>
-        <div className='border-x-1 border-slate-600 border-solid flex justify-between items-center w-full m-auto'>
+        <div className='border-x-1 border-slate-600 border-solid flex justify-between items-center w-full mt-auto'>
             <div className='flex justify-center items-center'>
-                <img src='https://minotar.net/avatar/JustTheWise' alt='Arka Group Icon' className='w-[30px] rounded' />
-                <p className='text-xs font-bold'>&nbsp;&nbsp;JustTheWise</p>
+                <img src={`https://minotar.net/avatar/${username}`} alt='Arka Group Icon' className='w-[30px] rounded' />
+                <p className='text-xs font-bold'>&nbsp;&nbsp;{username}</p>
             </div>
-            <button className={'border-none cursor-pointer text-slate-400 text-xl transition-all hover:text-white' + (page === '' ? ' text-white' : '')}  onClick={handlePageChange} data-page={''}>
+            <button className={'border-none cursor-pointer text-slate-400 text-xl transition-all hover:text-white' + (page.includes('Settings') ? ' text-white' : '')} onClick={handlePageChange} data-page={PAGES.SETTINGS_ACCOUNT}>
                 <FiSettings />
             </button>
         </div>
