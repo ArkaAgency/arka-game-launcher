001c844c70f654320ea6d26ee9d49df70d1a438b
AlexandreLG2022 <a.legal.contact@gmail.com>
2023-08-01 15:24:43 +0200

    :clown_face: Fixed mods api mock

--- src/__mocks__/modsApi.js
+++ src/__mocks__/modsApi.js
@@ -3,37 +3,37 @@ const mods = [
         name: 'SuperMod1',
         filename: 'test1.jar',
         type: 'required',
-        hash: 'hrthrjujtthrhtr'
+        md5: 'hrthrjujtthrhtr'
     },
     {
         name: 'SuperMod2',
         filename: 'test2.jar',
         type: 'required',
-        hash: 'g43gr3h3g3g34'
+        md5: 'g43gr3h3g3g34'
     },
     {
         name: 'SuperMod3',
         filename: 'test3.jar',
         type: 'required',
-        hash: 'h54h3533'
+        md5: 'h54h3533'
     },
     {
         name: 'SuperMod4',
         filename: 'test4.jar',
         type: 'required',
-        hash: 'g34g43g43g43'
+        md5: 'g34g43g43g43'
     },
     {
         name: 'SuperMod5',
         filename: 'test5.jar',
         type: 'optionnal',
-        hash: 'fewhfefh39248ffewdnoifwe'
+        md5: 'fewhfefh39248ffewdnoifwe'
     },
     {
         name: 'SuperMod5',
         filename: 'test7.jar',
         type: 'performances',
-        hash: 'fewdwqdewferwfew'
+        md5: 'fewdwqdewferwfew'
     }
 ];
 
